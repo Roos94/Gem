052042077115b820253c7227f3f0a72efbbf53a8
SteenBartholdy <s145090@student.dtu.dk>
2015-11-23 11:29:26 +0100

    skabelon

---
+++ app/src/main/java/com/dtu/smmac/gem/Hovedskaerm.java
@@ -0,0 +1,13 @@
+package com.dtu.smmac.gem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hovedskaerm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hovedskaerm);
+    }
+}
