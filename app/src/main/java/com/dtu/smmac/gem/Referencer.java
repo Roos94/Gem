a1955db5c599f56cde29b906cf3e7948a4a1327b
Anders <s140996@student.dtu.dk>
2015-11-23 14:00:40 +0100

    Reference-skaerm

---
+++ app/src/main/java/com/dtu/smmac/gem/Referencer.java
@@ -0,0 +1,13 @@
+package com.dtu.smmac.gem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Referencer extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referencer);
+    }
+}
