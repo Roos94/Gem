a6f27b7e533fb8b07d0c08ad1666a04e161392c3
Roos <s145094@student.dtu.dk>
2015-11-26 17:11:10 +0100

    Mino

--- app/src/main/java/com/dtu/smmac/gem/Hovedskaerm.java
+++ app/src/main/java/com/dtu/smmac/gem/Hovedskaerm.java
@@ -51,9 +51,9 @@ public class Hovedskaerm extends Activity implements AdapterView.OnItemClickList
 
         this.b1 = (Button) findViewById(R.id.beskrivelseb1);
 
-        //this.b2 = (Button) findViewById(R.id.beskrivelseb2);
+        this.b2 = (Button) findViewById(R.id.dateringb2);
 
-        //this.tv = (TextView) findViewById(R.id.overskriftBeskrivelse);
+        this.tv = (TextView) findViewById(R.id.overskriftDatering);
         this.tv.setText(genstand.getTitle());
 
         this.b1.setOnClickListener(this);
