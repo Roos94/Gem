00c9d34e1d60331554b149c6919713491b96fc32
Roos <s145094@student.dtu.dk>
2015-12-02 10:07:54 +0100

    lol

--- app/src/main/java/com/dtu/smmac/gem/Billede.java
+++ app/src/main/java/com/dtu/smmac/gem/Billede.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -20,10 +22,24 @@ public class Billede extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_billede);
 
+        this.getActionBar().setTitle("    " + "Taillede");
+
         b = (Button) findViewById(R.id.bbillede);
         iv = (ImageView) findViewById(R.id.iv_billede);
 
         b.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_topbar, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void done(MenuItem item)
+    {
+
     }
 
     @Override
