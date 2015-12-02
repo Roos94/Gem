8f66e056bce9083cee817ba7c429b9d7454a4a82
SteenBartholdy <s145090@student.dtu.dk>
2015-12-02 09:53:32 +0100

    billede

--- app/src/main/java/com/dtu/smmac/gem/Billede.java
+++ app/src/main/java/com/dtu/smmac/gem/Billede.java
@@ -1,36 +1,56 @@
 package com.dtu.smmac.gem;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.TabHost;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+public class Billede extends Activity implements View.OnClickListener {
 
-public class Billede extends Activity {
-
-    TabHost tabHost;
+    private ImageView iv;
+    private Button b;
+    private int REQUEST_CODE =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        tabHost = (TabHost) findViewById(R.id.tabHost);
+        b = (Button) findViewById(R.id.button_billede);
+        iv = (ImageView) findViewById(R.id.iv_billede);
 
-
-        tabHost.setup();
-
-        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Billede");
-
-        tabSpec.setContent(R.id.billede);
-        tabSpec.setIndicator("Billede");
-        tabHost.addTab(tabSpec);
-
-
-        tabHost.newTabSpec("Galleri");
-
-        tabSpec.setContent(R.id.galleri);
-        tabSpec.setIndicator("Galleri");
-        tabHost.addTab(tabSpec);
-
+        b.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(i.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(i, REQUEST_CODE);
+        }
+    }
+
+    public void onActivityResult (int requestcode, int resultcode, Intent data)
+    {
+        if(requestcode == REQUEST_CODE)
+        {
+            if(resultcode == RESULT_OK)
+            {
+                Bundle bundle = new Bundle();
+
+                bundle = data.getExtras();
+
+                Bitmap bit;
+
+                bit = (Bitmap) bundle.get("data");
+
+                iv.setImageBitmap(bit);
+
+            }
+        }
+    }
 }
