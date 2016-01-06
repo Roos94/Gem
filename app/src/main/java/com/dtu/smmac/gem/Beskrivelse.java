a1ca2695d6233cf9a094b45d30563130a324068b
ChristofferSvendsen <s145089@student.dtu.dk>
2016-01-06 15:50:16 +0100

    Beskrivelse, optage og afspille lyd

--- app/src/main/java/com/dtu/smmac/gem/Beskrivelse.java
+++ app/src/main/java/com/dtu/smmac/gem/Beskrivelse.java
@@ -1,13 +1,18 @@
 package com.dtu.smmac.gem;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Beskrivelse extends Activity implements View.OnClickListener {
 
@@ -16,7 +21,9 @@ public class Beskrivelse extends Activity implements View.OnClickListener {
     private ImageButton record, play;
     private EditText beskrivelse;
     private int rec, pl; // Bruges til at holde styr på hvilket billede der vises
-    private MediaRecorder recorder = new MediaRecorder();
+    private MediaRecorder recorder;
+    private MediaPlayer player;
+    private String OUTPUT_FILE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,10 @@ public class Beskrivelse extends Activity implements View.OnClickListener {
 
         beskrivelse.setText("Hej"); // Skal hentes den gemte tekst fra databasen
         beskrivelse.setSelection(beskrivelse.getText().length()); //Sætter cursor ved slutningen af teksten
+
+
+        OUTPUT_FILE = Environment.getExternalStorageDirectory() + "/audiorecorder.3gpp";
+
     }
 
     @Override
@@ -62,6 +73,13 @@ public class Beskrivelse extends Activity implements View.OnClickListener {
             // Skifter mellem de to billeder (mic og stop)
             if (rec == 1) {
                 record.setImageResource(R.drawable.rcircle);
+
+                try {
+                    beginRecording();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 this.rec = 2;
                 // Starter lydoptagelse
             }
@@ -69,6 +87,13 @@ public class Beskrivelse extends Activity implements View.OnClickListener {
             else
             {
                 record.setImageResource(R.drawable.mic);
+
+                try {
+                    stopRecording();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 this.rec = 1;
                 // Stopper lydoptagelsen
             }
@@ -79,17 +104,83 @@ public class Beskrivelse extends Activity implements View.OnClickListener {
             // Skifter mellem de to billeder (mic og stop)
             if (this.pl == 1) {
                 play.setImageResource(R.drawable.rcircle);
+
+                try {
+                    playRecording();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 this.pl = 2;
                 // Starter afspilningen
             }
 
             else {
                 play.setImageResource(R.drawable.play);
+
+                try {
+                    stopPlayback();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 this.pl = 1;
                 // Stopper afspilningen
             }
         }
 
+    }
+
+    private void beginRecording() throws IOException {
+        ditchRecord();
+        File outFile = new File(OUTPUT_FILE);
+
+        if(outFile.exists())
+            outFile.delete();
+
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);
+        recorder.setOutputFile(OUTPUT_FILE);
+        recorder.prepare();
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        if(recorder != null)
+            recorder.stop();
+    }
+
+    private void playRecording() throws IOException {
+        ditchPlayer();
+
+        player = new MediaPlayer();
+        player.setDataSource(OUTPUT_FILE);
+        player.prepare();
+        player.start();
+    }
+
+    private void stopPlayback() {
+        if(player != null)
+            player.stop();
+    }
+
+    private void ditchRecord() {
+        if(recorder != null)
+            recorder.release();
+    }
+
+    private void ditchPlayer() {
+        if(player != null)
+        {
+            try {
+                player.release();
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
