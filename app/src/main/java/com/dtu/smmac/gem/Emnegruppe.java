457a29e79ce3d66e602e95d67b4c67abf3ef5466
Roos <s145094@student.dtu.dk>
2016-01-08 09:55:23 +0100

    Emnegruppe

--- app/src/main/java/com/dtu/smmac/gem/Emnegruppe.java
+++ app/src/main/java/com/dtu/smmac/gem/Emnegruppe.java
@@ -3,6 +3,7 @@ package com.dtu.smmac.gem;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,11 +23,16 @@ import java.util.ArrayList;
 
 public class Emnegruppe extends Activity implements AdapterView.OnItemClickListener {
 
-    ListView lv;
-    Intent intent;
+    private ListView lv;
     private String emne;
     private TextView emnet;
 
+    private Intent h;
+    private int ID;
+    private Intent lastUsed;
+    private int genstandID;
+
+    private boolean done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,8 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
         setContentView(R.layout.activity_emnegruppe);
 
         this.getActionBar().setTitle("    " + "Emnegruppe");
+
+        this.done = true;
 
         this.emnet = (TextView) findViewById(R.id.editText2);
 
@@ -48,13 +56,27 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
         emnegruppe.add("Gamle ting");
         emnegruppe.add("Fifa");
         emnegruppe.add("Playstation");
-        emnegruppe.add("Tilføj Andet");
+        emnegruppe.add("Xbox");
+        emnegruppe.add("Computer");
+        emnegruppe.add("Batteri");
+        emnegruppe.add("Disk");
 
-        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, emnegruppe));
+        this.lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, emnegruppe));
 
-        lv.setOnItemClickListener(this);
+        this.lv.setOnItemClickListener(this);
 
-        emne = ""; // Hentes fra databasen
+        //Sætter HS
+        this.h = new Intent(this, Hovedskaerm.class);
+
+        //Trækker fra HS
+        this.lastUsed = getIntent();
+        this.ID = this.lastUsed.getIntExtra("ID", 0);
+
+        setGenstand(this.ID);
+
+        this.emne = Splash.genstand.getGenstandList().get(this.genstandID).getEmnegruppe();
+
+        this.emnet.setText("Valgt emne: " + this.emne);
     }
 
     @Override
@@ -65,14 +87,62 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
     }
 
     public void done(MenuItem item) {
-        System.out.println(emne); // skal sendes til databasen
-        finish();
+        if(this.done == true)
+        {
+            this.done = false;
+
+            new AsyncTask() {
+                @Override
+                protected Object doInBackground(Object[] params) {
+                    try {
+                        Splash.genstand.setEmnegruppe(ID, emne);
+                        Splash.genstand.setGenstandList();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Object resultat)
+                {
+                    Startskaerm.adap.notifyDataSetChanged();
+                    startHS();
+                }
+            }.execute();
+        }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         emne = lv.getItemAtPosition(position).toString();
         emnet.setText("Valgt emne: " + emne);
+    }
+
+    public void setGenstand(int ID)
+    {
+        for (this.genstandID = 0; this.genstandID < Splash.genstand.getGenstandList().size(); this.genstandID++) {
+            if (Splash.genstand.getGenstandList().get(this.genstandID).getID() == ID)
+            {
+                return;
+            }
+        }
+    }
+
+    public void startHS()
+    {
+        //Genstand skal køres over på h
+        h.putExtra("ID", this.ID);
+
+        startActivity(h);
+
+        finish();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        startHS();
     }
 
 
