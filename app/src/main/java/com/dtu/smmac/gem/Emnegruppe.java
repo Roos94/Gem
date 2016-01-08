5c25549047979c8b3cdbdf8915736a5fda5c195a
ChristofferSvendsen <s145089@student.dtu.dk>
2016-01-08 09:40:27 +0100

    Emnegruppe rettet

--- app/src/main/java/com/dtu/smmac/gem/Emnegruppe.java
+++ app/src/main/java/com/dtu/smmac/gem/Emnegruppe.java
@@ -3,7 +3,6 @@ package com.dtu.smmac.gem;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +11,7 @@ import android.widget.EditText;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -22,16 +22,10 @@ import java.util.ArrayList;
 
 public class Emnegruppe extends Activity implements AdapterView.OnItemClickListener {
 
-    private ListView lv;
+    ListView lv;
+    Intent intent;
     private String emne;
-    private EditText emnet;
-
-    private Intent h;
-    private int ID;
-    private Intent lastUsed;
-    private int genstandID;
-
-    private boolean done;
+    private TextView emnet;
 
 
     @Override
@@ -41,11 +35,9 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
 
         this.getActionBar().setTitle("    " + "Emnegruppe");
 
-        this.done = true;
+        this.emnet = (TextView) findViewById(R.id.editText2);
 
-        this.emnet = (EditText) findViewById(R.id.editText2);
-
-        this.lv = (ListView) findViewById(R.id.lv);
+        lv = (ListView) findViewById(R.id.lv);
 
         ArrayList emnegruppe = new ArrayList();
 
@@ -56,27 +48,13 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
         emnegruppe.add("Gamle ting");
         emnegruppe.add("Fifa");
         emnegruppe.add("Playstation");
-        emnegruppe.add("Xbox");
-        emnegruppe.add("Computer");
-        emnegruppe.add("Batteri");
-        emnegruppe.add("Disk");
+        emnegruppe.add("Tilføj Andet");
 
-        this.lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, emnegruppe));
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, emnegruppe));
 
-        this.lv.setOnItemClickListener(this);
+        lv.setOnItemClickListener(this);
 
-        //Sætter HS
-        this.h = new Intent(this, Hovedskaerm.class);
-
-        //Trækker fra HS
-        this.lastUsed = getIntent();
-        this.ID = this.lastUsed.getIntExtra("ID", 0);
-
-        setGenstand(this.ID);
-
-        this.emne = Splash.genstand.getGenstandList().get(this.genstandID).getEmnegruppe();
-
-        this.emnet.setText("Valgt emne: " + this.emne);
+        emne = ""; // Hentes fra databasen
     }
 
     @Override
@@ -87,62 +65,15 @@ public class Emnegruppe extends Activity implements AdapterView.OnItemClickListe
     }
 
     public void done(MenuItem item) {
-        if(this.done == true)
-        {
-            this.done = false;
-
-            new AsyncTask() {
-                @Override
-                protected Object doInBackground(Object[] params) {
-                    try {
-                        Splash.genstand.setEmnegruppe(ID, emne);
-                        Splash.genstand.setGenstandList();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Object resultat)
-                {
-                    Startskaerm.adap.notifyDataSetChanged();
-                    startHS();
-                }
-            }.execute();
-        }
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        this.emne = lv.getItemAtPosition(position).toString();
-        this.emnet.setText("Valgt emne: " + this.emne);
-    }
-
-    public void setGenstand(int ID)
-    {
-        for (this.genstandID = 0; this.genstandID < Splash.genstand.getGenstandList().size(); this.genstandID++) {
-            if (Splash.genstand.getGenstandList().get(this.genstandID).getID() == ID)
-            {
-                return;
-            }
-        }
-    }
-
-    public void startHS()
-    {
-        //Genstand skal køres over på h
-        h.putExtra("ID", this.ID);
-
-        startActivity(h);
-
+        System.out.println(emne); // skal sendes til databasen
         finish();
     }
 
     @Override
-    public void onBackPressed()
-    {
-        startHS();
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        emne = lv.getItemAtPosition(position).toString();
+        emnet.setText("Valgt emne: " + emne);
     }
+
 
 }
