c12455bb1b0df147c1889b8733f4915439055214
Roos <s145094@student.dtu.dk>
2016-01-18 11:24:25 +0100

    kommentarer

--- app/src/main/java/com/dtu/smmac/gem/Activity/Splash.java
+++ app/src/main/java/com/dtu/smmac/gem/Activity/Splash.java
@@ -28,17 +28,22 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //Sætter intent - Åbner Main klassen
         this.i = new Intent(this, Main.class);
 
+        //Sætter logoet
         this.img = (ImageView) findViewById(R.id.imageSplash);
         this.img.setImageResource(R.drawable.logo);
 
+        //Sætter app navnet
         this.titel = (TextView) findViewById(R.id.textSplah);
         this.titel.setText(R.string.app_name);
 
+        //Sætter "Made by"-teksten
         this.lille = (TextView) findViewById(R.id.lilleSplash);
         this.lille.setText("Made by: SMMAC");
 
+        //Chekker om telefonen er på nettet
         if(isNetworkAvailable() == true) {
             setInfo();
         }
@@ -52,17 +57,23 @@ public class Splash extends Activity {
 
     public void setInfo()
     {
+        //Opretter DAO'en
         this.DB = new DAO();
 
+        //Sætter runable
         setRunable();
 
+        //Sætter en ny tråd
         this.timer = new Thread(this.r);
 
         new AsyncTask() {
             @Override
             protected Object doInBackground(Object[] params) {
                 try {
+                    //Henter item i DAO
                     DB.setItemList();
+
+                    //Starter timeren
                     timer.start();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -134,6 +145,7 @@ public class Splash extends Activity {
         };
     }
 
+    //Bruges til at se om der er netværk
     public boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
