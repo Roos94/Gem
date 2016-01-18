4e029ebc8bc67325c88de4feb6608e8c626dcab0
Roos <s145094@student.dtu.dk>
2016-01-18 10:03:26 +0100

    kommentarer

--- app/src/main/java/com/dtu/smmac/gem/Adapter/ItemView_adapter.java
+++ app/src/main/java/com/dtu/smmac/gem/Adapter/ItemView_adapter.java
@@ -12,6 +12,8 @@ import com.dtu.smmac.gem.Items.Item;
 import com.dtu.smmac.gem.R;
 
 /**
+ * Denne klasse bruges til at sætte adapteren på listen i ItemView aktiviteten
+ *
  * Created by Roos on 02/12/15.
  */
 public class ItemView_adapter extends ArrayAdapter<Item> {
@@ -19,16 +21,19 @@ public class ItemView_adapter extends ArrayAdapter<Item> {
     private Item item;
     private Context context;
 
+    //Konstruktøren
     public ItemView_adapter(Context c, Item item) {
         super(c, R.layout.row_itemview);
         this.item = item;
         this.context = c;
     }
 
+    //Returnerer antallet af rækker i listen
     public int getCount() {
         return 7;
     }
 
+    //Sætter view'et for hver række i listen
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View row = convertView;
@@ -55,23 +60,27 @@ public class ItemView_adapter extends ArrayAdapter<Item> {
             holder = (Holder) row.getTag();
         }
 
+        //Sææter billede
         if (position == 0) {
             holder.imgView.setImageResource(item.getImage());
             holder.titleView.setText("Billede");
             holder.besView.setText("Antal billeder: 1");
         }
+
+        //Sætter Emnegruppe
         else if (position == 1) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Emnegruppe");
             holder.besView.setText(this.item.getEmnegruppe());
         }
+
+        //Sætter Modtagelsesdato
         else if (position == 2) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Modtagelsesdato");
             holder.besView.setText(this.item.getModtaget());
         }
+
+        //Sætter betegnelse
         else if (position == 3) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Betegnelse");
 
             String bet = this.item.getBetegnelse();
@@ -83,13 +92,15 @@ public class ItemView_adapter extends ArrayAdapter<Item> {
 
             holder.besView.setText(bet);
         }
+
+        //Sætter Datering
         else if (position == 4) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Datering");
             holder.besView.setText("Fra: " + this.item.getDateringFra() + "\n" + "Til: " + this.item.getDateringTil());
         }
+
+        //Sætter beskrivelse
         else if (position == 5) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Beskrivelse");
 
             String bes = this.item.getBeskrivelse();
@@ -101,8 +112,9 @@ public class ItemView_adapter extends ArrayAdapter<Item> {
 
             holder.besView.setText(bes);
         }
+
+        //Sætter referencer
         else if (position == 6) {
-            //holder.imgView.setImageResource();
             holder.titleView.setText("Referencer");
             holder.besView.setText("Donator: " + this.item.getDonator() + "\n" + "Producent: " + this.item.getProducer());
         }
@@ -110,6 +122,7 @@ public class ItemView_adapter extends ArrayAdapter<Item> {
         return row;
     }
 
+    //Klassen der håndterer hver række
     private static class Holder {
         public TextView titleView;
         public TextView besView;
